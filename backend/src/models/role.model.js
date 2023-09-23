71fd7e849c586e3f192ae45d3adb9fc7a9f6963b
Camilo Sáez Garrido <camjasaez@gmail.com>
2023-09-23 15:48:42 -0300

    Modelos actualizados

--- backend/src/models/role.model.js
+++ backend/src/models/role.model.js
@@ -5,13 +5,18 @@ const mongoose = require("mongoose");
 const ROLES = ["user", "admin"];
 
 // Crea el esquema de la coleccion 'roles'
-const roleSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    enum: ROLES,
-    required: true,
+const roleSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      enum: ROLES,
+      required: true,
+    },
   },
-});
+  {
+    versionKey: false,
+  },
+);
 
 // Crea el modelo de datos 'Role' a partir del esquema 'roleSchema'
 const Role = mongoose.model("Role", roleSchema);
