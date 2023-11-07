f14b691a62cfd65c89bcaffea9ee9024e6e50365
Camilo Sáez Garrido <camjasaez@gmail.com>
2023-11-07 01:01:06 -0300

    Fix main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom/client';
 import App from './routes/App.jsx';
 import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Root from './routes/root.jsx';
+import Root from './routes/Root.jsx';
 import ErrorPage from './routes/ErrorPage.jsx';
 import Login from './routes/Login.jsx';
 
--- frontend/src/services/root.service.js
+++ frontend/src/services/root.service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_URL = import.meta.env.VITE_BASE_URL || 'http://localhost:5000/api';
-console.log(API_URL);
+
 const instance = axios.create({
   baseURL: API_URL,
   headers: {
